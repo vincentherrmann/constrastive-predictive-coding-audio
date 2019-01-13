eb3a1ca929c45f8cfa6b9cbca4d862639737e06b
vincentherrmann <vincent.herrmann@web.de>
2019-01-14 00:48:30 +0100

    add cuda option to positional encoder

--- attention_model.py
+++ attention_model.py
@@ -12,7 +12,10 @@ class PositionalEncoder(nn.Module):
 
         # create constant 'pe' matrix with values dependant on
         # pos and i
+
         pe = torch.zeros([max_seq_len, code_size], requires_grad=False)
+        if torch.cuda.is_available():
+            pe = torch.zeros([max_seq_len, code_size], requires_grad=False).cuda()
         for pos in range(max_seq_len):
             for i in range(0, code_size, 2):
                 pe[pos, i] = math.sin(pos / (10000 ** ((2 * i) / code_size)))
