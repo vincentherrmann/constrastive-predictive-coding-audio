06442f7ab1069118e409bfe38b0077eaf3f7295c
vincentherrmann <vincent.herrmann@web.de>
2019-01-24 10:55:11 +0100

    use log sscalogram

--- scalogram_model.py
+++ scalogram_model.py
@@ -74,7 +74,7 @@ class ScalogramEncoder(nn.Module):
 
     def forward(self, x):
         x = self.cqt(x)
-        x = abs(x).unsqueeze(1)
+        x = torch.log(torch.pow(abs(x), 2)).unsqueeze(1)
         for i, module in enumerate(self.module_list):
             x = module(x)
             #print("shape after module", i, " - ", x.shape)
@@ -139,7 +139,7 @@ class ScalogramSeperableEncoder(nn.Module):
 
     def forward(self, x):
         x = self.cqt(x)
-        x = abs(x).unsqueeze(1)
+        x = torch.log(torch.pow(abs(x), 2)).unsqueeze(1)
         for i, module in enumerate(self.module_list):
             x = module(x)
             #print("shape after module", i, " - ", x.shape)
