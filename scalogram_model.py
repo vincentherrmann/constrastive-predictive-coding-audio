53b0c1bdc1ae3d45db5c107bffa2f73a4be0687f
vincentherrmann <vincent.herrmann@web.de>
2019-01-28 21:19:12 +0100

    fix seperable convolution

--- scalogram_model.py
+++ scalogram_model.py
@@ -127,7 +127,7 @@ class Conv2dSeperable(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, bias=True):
         super().__init__()
         self.conv = nn.Conv2d(in_channels=in_channels, out_channels=in_channels, kernel_size=kernel_size,
-                              stride=stride, padding=padding, dilation=dilation, bias=False)
+                              stride=stride, padding=padding, dilation=dilation, bias=False, groups=in_channels)
         self.conv_1x1 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, bias=bias)
 
     @property
