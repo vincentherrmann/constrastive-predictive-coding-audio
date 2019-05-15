5b1667a2fb629adc7f77be7b473ab2ceed72f084
vincentherrmann <vincent.herrmann@web.de>
2019-05-15 13:28:46 +0200

    add experiment 23

--- configs/experiment_configs.py
+++ configs/experiment_configs.py
@@ -108,6 +108,9 @@ experiments['e22'] = experiments['e18'].copy()
 experiments['e22']['encoder_config'] = scalogram_resnet_configs.scalogram_resnet_architecture_7
 experiments['e22']['ar_model_config'] = autoregressive_model_configs.ar_conv_architecture_3
 
+experiments['e23'] = experiments['e22'].copy()
+experiments['e23']['training_config']['wasserstein_gradient_penalty'] = False
+
 
 experiments['c1'] = classification_default_dict
 experiments['c1']['training_config']['learning_rate'] = 1e-3
